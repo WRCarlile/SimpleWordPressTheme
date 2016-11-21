2ebe93960b24a5468f945a732b2c222bbb32b3d8
“Ryan <ryancarlile@gmail.com>
2016-11-21 12:58:58 -0800

    Finish simple wordpress theme tutorial

--- page.php
+++ page.php
@@ -6,22 +6,15 @@
 		<?php while (have_posts()) : the_post(); ?>
 		<article class="post">
 			<h3>
-				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+			<?php the_title(); ?>
 			</h3>
-			<div class="meta">
-				Created By
-				<?php the_author(); ?> on
-				<?php the_time('F j, Y g:i a'); ?>
-			</div>
 			<?php if( has_post_thumbnail()) : ?>
 			<div class="post-thumbnail">
 				<?php the_post_thumbnail(); ?>
 
 			</div>
 			<?php endif; ?>
-			<?php the_excerpt(); ?>
-			<br>
-			<a class="button" href="<?php the_permalink(); ?>">Read More</a>
+			<?php the_content(); ?>
 
 		</article>
 		<?php endwhile; ?>
@@ -29,5 +22,5 @@
 		<?php echo wpautop('Sorry, No Posts were found'); ?>
 		<?php endif; ?>
 	</div>
-
-	<?php get_footer(); ?>
+</div>
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
 <div class="container">
 	<div class="main">
-
 		<?php if (have_posts()) : ?>
 		<?php while (have_posts()) : the_post(); ?>
 		<article class="post">
 			<h3>
-				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				<?php the_title(); ?>
 			</h3>
 			<div class="meta">
 				Created By
@@ -16,18 +15,15 @@
 			<?php if( has_post_thumbnail()) : ?>
 			<div class="post-thumbnail">
 				<?php the_post_thumbnail(); ?>
-
 			</div>
 			<?php endif; ?>
-			<?php the_excerpt(); ?>
-			<br>
-			<a class="button" href="<?php the_permalink(); ?>">Read More</a>
-
+			<?php the_content(); ?>
 		</article>
 		<?php endwhile; ?>
 		<?php else : ?>
 		<?php echo wpautop('Sorry, No Posts were found'); ?>
 		<?php endif; ?>
+		<?php comments_template(); ?>
 	</div>
 
 	<?php get_footer(); ?>
